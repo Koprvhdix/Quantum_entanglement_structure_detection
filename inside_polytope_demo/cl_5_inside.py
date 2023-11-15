7a0422986519f06752e8b43201bed0401498d235
Koprvhdix <koprvhdix@gmail.com>
2023-11-15 19:45:44 +0800

    fix

---
+++ inside_polytope_demo/cl_5_inside.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+from ML_PIC import ML_PIC
+from partition_tools import generate_k_partitionable_partitions
+
+if __name__ == "__main__":
+    rho = np.zeros((32, 32))
+
+    indices = [0, 15, 19, 28]
+    for index in indices:
+        for index2 in indices:
+            rho[index, index2] = 0.25
+
+    partition_list = generate_k_partitionable_partitions(5, 4)
+    # partition_list = generate_k_partitionable_partitions(5, 3)
+    # partition_list = generate_k_partitionable_partitions(5, 2)
+    current_class = ML_PIC(5, 300, rho, partition_list, 1)
+    current_class.train(2000)
+    current_class.sdp()
